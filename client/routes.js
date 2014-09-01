c871e20c5d3895149fc557e0d0be17585af4912e
dan <dan>
2014-09-01 12:52:15 -0700

    big in battle notifications

--- client/routes.js
+++ client/routes.js
@@ -1,5 +1,17 @@
 Router.map(function() {
+	// this.route('index', {
+	// 	path: '/',
+	// 	action: function() {
+	// 		if (Meteor.userId) {
+	// 			this.render('game')
+	// 		} else {
+	// 			this.render('landing')
+	// 		}
+	// 	}
+	// })
+
 	this.route('game', {path: '/'})
+
 	this.route('admin')
 
 	this.route('admin_forums', {
