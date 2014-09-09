14b22649fbed29b8bcd8d6a3061bd661f7aaceb4
dan <danphi@gmail.com>
2014-09-09 12:11:30 -0700

    fix

--- stripe_temp.js
+++ stripe_temp.js
@@ -1,8 +1,8 @@
 // duplicate this file, rename to "stripe.js" and fill in your keys
 // "stripe.js" is in the .gitignore and not checked in
 
-stripe_publishable_key = 'pk_test_cAwCeMfcNMJcnFldsBhabfFC'
+stripe_publishable_key = ''
 
 if (Meteor.isServer) {
-	stripe_secret_key = 'sk_test_vIn38YEDRNnSm2f3W3SMsQKb'
+	stripe_secret_key = ''
 }
