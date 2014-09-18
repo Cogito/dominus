037520ad87a968e8cf68d22ab85dd9abadf0ce09
dan <danphi@gmail.com>
2014-09-18 12:31:52 -0700

    added browser policy

--- server/startup.js
+++ server/startup.js
@@ -1,1 +1,6 @@
 Future = Npm.require('fibers/future')
+
+BrowserPolicy.content.allowOriginForAll("http://www.google-analytics.com")
+BrowserPolicy.content.allowOriginForAll("http://netdna.bootstrapcdn.com")
+BrowserPolicy.content.allowOriginForAll("https://*.stripe.com")
+BrowserPolicy.content.allowEval("https://*.stripe.com")
