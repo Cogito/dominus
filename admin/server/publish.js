8f250f6792875e16580a97057711ca2a284d5b4d
Andrew Ardill <andrew.ardill@gmail.com>
2014-11-28 17:24:02 +1100

    admin: only publish admin collections to the admin users
    Previously anyone could subscribe to these collections, exposing data to
    non-admins. This was hidden in the UI, however the collections should
    not be available in the first place.
    
    Create a wrapper to make publishing admin collections standardised.
    
    Move publish functions to the same location.

--- admin/server/publish.js
+++ admin/server/publish.js
@@ -1,19 +1,37 @@
-Meteor.publish('admin_forums', function() {
+var admin_publish = function (name, handler) {
+	Meteor.publish(name, function() {
+		if(this.userId && get_user_property("admin", this.userId)) {
+			return handler()
+		} else {
+			this.ready()
+		}
+	})
+}
+
+admin_publish('admin_forums', function() {
 	return Forums.find()
 })
 
-Meteor.publish('admin_charges', function() {
+admin_publish('admin_charges', function() {
 	return Charges.find()
 })
 
-Meteor.publish('admin_gamestats', function() {
+admin_publish('admin_gamestats', function() {
 	return Gamestats.find()
 })
 
-Meteor.publish('admin_jobstats', function() {
+admin_publish('admin_jobstats', function() {
 	return Jobstats.find()
 })
 
 Meteor.publish('admin_jobqueue', function() {
 	return Jobqueue.find()
+})
+
+admin_publish('admin_mailchimp_list', function() {
+	return Meteor.users.find({}, {fields: {username:1, emails:1}})
+})
+
+Meteor.publish('admin_users_online', function() {
+	return Meteor.users.find({"status.online":true}, {fields: {username:1, "status.lastLogin.date":1, "status.online":1}})
 })
