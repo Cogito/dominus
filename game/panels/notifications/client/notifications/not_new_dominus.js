69bc1803ed6f14c171fff30778e050525729c679
Andrew Ardill <andrew.ardill@gmail.com>
2014-12-03 17:41:08 +1100

    notification_new_dominus: Wait for data to be ready before rendering
    This is probably better handled in the parent template, when we provide
    the data context. Here will do for now!

--- game/panels/notifications/client/notifications/not_new_dominus.js
+++ game/panels/notifications/client/notifications/not_new_dominus.js
@@ -1,5 +1,16 @@
 Template.not_new_dominus.helpers({
 	newDominus: function() {
 		return this.vars._id === Meteor.userId()
+	},
+	ready: function () {
+		return Template.instance().subReady.get()
 	}
 })
+
+Template.not_new_dominus.created = function() {
+	var self = this
+	self.subReady = new ReactiveVar(false)
+	self.autorun(function() {
+		self.subReady.set(Meteor.subscribe("a_notification").ready())
+	})
+}
