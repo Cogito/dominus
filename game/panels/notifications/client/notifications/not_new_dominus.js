cef6899145545b91374dc902e0e625ca54e78711
Andrew Ardill <andrew.ardill@gmail.com>
2014-12-03 17:43:40 +1100

    notification_new_dominus: Improve notification messaging
    Uses the old Dominus to work out what a user's relationship to the
    notification is when rendering. Could be extended to include the
    what the game clock was set to at the time the notification was made.

--- game/panels/notifications/client/notifications/not_new_dominus.js
+++ game/panels/notifications/client/notifications/not_new_dominus.js
@@ -1,6 +1,17 @@
+var isDominus = function() {
+	return this.vars._id == Meteor.userId()
+}
+var isLastDominus = function() {
+	return this.vars.oldDominusId == Meteor.userId()
+}
 Template.not_new_dominus.helpers({
-	newDominus: function() {
-		return this.vars._id === Meteor.userId()
+	isDominus: isDominus,
+	isLastDominus: isLastDominus,
+	sameDominus: function() {
+		return this.vars.oldDominusId == this.vars._id
+	},
+	notDominus: function() {
+		return !isDominus.call(this) && !isLastDominus.call(this)
 	},
 	ready: function () {
 		return Template.instance().subReady.get()
