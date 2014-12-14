d9a187da85489e7805208e0de89521b257577c71
dan <danphi@gmail.com>
2014-12-13 22:08:55 -0800

    added summary panel icons to lords and vassals

--- game/panels/left_panel/client/lp_allies.js
+++ game/panels/left_panel/client/lp_allies.js
@@ -3,3 +3,17 @@ Template.lp_allies.helpers({
 		return LeftPanelAllies.find({}, {sort:{income:-1}})
 	}
 })
+
+
+Template.lp_ally.helpers({
+	inBattle: function() {
+		return Battles.findOne({x:this.x, y:this.y}, {fields: {_id:1}})
+	},
+})
+
+
+Template.lp_ally.created = function() {
+	if (Template.currentData()) {
+		Meteor.subscribe('battle_notifications_at_hex', Template.currentData().x, Template.currentData().y)
+	}
+}
--- game/panels/left_panel/client/lp_lords.js
+++ game/panels/left_panel/client/lp_lords.js
@@ -3,3 +3,16 @@ Template.lp_lords.helpers({
 		return LeftPanelLords.find({}, {sort:{income:-1}})
 	}
 })
+
+Template.lp_lord.helpers({
+	inBattle: function() {
+		return Battles.findOne({x:this.x, y:this.y}, {fields: {_id:1}})
+	},
+})
+
+
+Template.lp_lord.created = function() {
+	if (Template.currentData()) {
+		Meteor.subscribe('battle_notifications_at_hex', Template.currentData().x, Template.currentData().y)
+	}
+}
