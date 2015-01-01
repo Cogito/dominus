7b8aa8afe812302c9fb7f8acdd4be2c4bb721593
dan <danphi@gmail.com>
2015-01-01 11:18:01 -0800

    graphs

--- admin/server/gamestats.js
+++ admin/server/gamestats.js
@@ -2,7 +2,7 @@
 gamestats_job = function() {
 	var start_time = new Date()
 
-	var stat = Gamestats.findOne({created_at: {$gte: s.statsBegin(), $lt: s.statsEnd()}})
+	var stat = Gamestats.findOne({created_at: {$gte: statsBegin(), $lt: statsEnd()}})
 
 	if (!stat) {
 		var stat = {
