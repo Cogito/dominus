6e2640ae44ed5382da7a10e8fefc01b13fd4cb91
dan <danphi@gmail.com>
2015-01-11 13:15:14 -0800

    position of connection lost alert

--- game/miscTemplates/connection_lost/client/connection_lost_alert.js
+++ game/miscTemplates/connection_lost/client/connection_lost_alert.js
@@ -9,14 +9,14 @@ Template.connection_lost_alert.rendered = function() {
 var canvasSize = Session.get('canvas_size')
     if (canvasSize) {
         $('#connectionLostPositioner').css('left', canvasSize.width/2 - 200)
-        $('#connectionLostPositioner').css('bottom', 20)
+        $('#connectionLostPositioner').css('top', canvasSize.height/2 - 150)
     }
 
     this.autorun(function() {
         var canvasSize = Session.get('canvas_size')
         if (canvasSize) {
             $('#connectionLostPositioner').css('left', canvasSize.width/2 - 200)
-            $('#connectionLostPositioner').css('bottom', 20)
+            $('#connectionLostPositioner').css('top', canvasSize.height/2 - 150)
         }
     })
 }
