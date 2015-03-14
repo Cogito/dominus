bbf3e5c439e5ffcafe0c8966ce001b86477613af
dan <danphi@gmail.com>
2015-03-14 13:10:50 -0700

    open registration

--- game/lib/settings_client.js
+++ game/lib/settings_client.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
 	s.game_name = 'Dominus'
 	s.game_number = 12
-	s.game_start_date = moment("2015-03-14 1:00 pm -0800", "YYYY-MM-DD h:m a Z").toDate()
+	s.game_start_date = moment("2015-03-14 1:00 pm -0700", "YYYY-MM-DD h:m a Z").toDate()
 
 	s.hex_move_speed = 12		// how fast does the grid move when moving
 	s.hex_scale_max = 1
--- server/accounts.js
+++ server/accounts.js
@@ -1,10 +1,10 @@
-Accounts.validateNewUser(function(user) {
-	if (process.env.DOMINUS_WORKER == 'true') {
-		return true
-	} else {
-		throw new Meteor.Error(503,'Dominus is full.  Please come back later.')
-	}
-})
+// Accounts.validateNewUser(function(user) {
+// 	if (process.env.DOMINUS_WORKER == 'true') {
+// 		return true
+// 	} else {
+// 		throw new Meteor.Error(503,'Dominus is full.  Please come back later.')
+// 	}
+// })
 
 
 // this is called before validateNewUser()
