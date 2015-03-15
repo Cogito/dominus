39f45542602fe095483c8795fab98215619d9098
dan <danphi@gmail.com>
2015-03-15 15:42:54 -0700

    add update everyone's relationships job

--- game/lib/server/functions/relation_finder.js
+++ game/lib/server/functions/relation_finder.js
@@ -7,154 +7,131 @@
 // team - everyone under your king not including self
 
 
+Cue.addJob('updateAllKingsAllies', {retryOnError:false, maxMs:1000*60*10}, function(task, done) {
+	Meteor.users.find({is_king:true}).forEach(function(user) {
+		var rf = new relation_finder(user._id);
+		rf.start();
+	});
+	done();
+});
+
+
 Cue.addJob('update_allies', {retryOnError:false, maxMs:1000*60*5}, function(task, done) {
-	var rf = new relation_finder(task.data.user_id)
-	rf.start()
+	var rf = new relation_finder(task.data.user_id);
+	rf.start();
 
 	// delay these to give everything a chance to finish
-	Cue.addTask('enemies_together_check', {isAsync:false, unique:true, delay:1000*60}, {})
-	Cue.addTask('enemy_on_building_check', {isAsync:false, unique:true, delay:1000*60}, {})
-	done()
-})
+	Cue.addTask('enemies_together_check', {isAsync:false, unique:true, delay:1000*60}, {});
+	Cue.addTask('enemy_on_building_check', {isAsync:false, unique:true, delay:1000*60}, {});
+	done();
+});
 
 
 relation_finder = function(user_id) {
-	check(user_id, String)
+	check(user_id, String);
 
-	this.user_id = user_id
-	this.user = Meteor.users.findOne(user_id, {fields: {lord:1}})
+	this.user_id = user_id;
+	this.user = Meteor.users.findOne(user_id, {fields: {lord:1}});
 	if (!this.user) {
-		throw new Meteor.Error(404, "Can't find user")
+		throw new Meteor.Error(404, "Can't find user");
 	}
-	this.num_branches = 1
-	this.team = []
-	this.update_cache = []
+	this.num_branches = 1;
+	this.team = [];
+	this.update_cache = [];
 
 
 
 	this.start = function() {
-		var self = this
-		self.find_top(self.user)
-	}
+		var self = this;
+		self.find_top(self.user);
+	};
 
 	this.find_top = function(user) {
-		check(user, Object)
+		check(user, Object);
 
-		var self = this
-		var lord = Meteor.users.findOne(user.lord, {fields: {lord:1}})
+		var self = this;
+		var lord = Meteor.users.findOne(user.lord, {fields: {lord:1}});
 		if (lord) {
 			if (user._id == lord._id) {
-				console.log('user: '+user.username)
-				console.log('lord: '+lord.username)
+				console.log('user: '+user.username);
+				console.log('lord: '+lord.username);
 				throw new Meteor.Error(404, "infinite loop");
 			}
-			self.find_top(lord)
+			self.find_top(lord);
 		} else {
-			self.traverse_down(user)
+			self.traverse_down(user);
 		}
-	}
+	};
 
 	// find all the branch ends
 	// add to team
 	// keep track of num_branches so that we know when we're totally done
 	this.traverse_down = function(user) {
-		check(user, Object)
-		var self = this
+		check(user, Object);
+		var self = this;
 
 		// if this is not me add to team
-		self.team.push(user._id)
+		self.team.push(user._id);
 
-		var vassals = Meteor.users.find({lord:user._id}, {fields: {lord:1}})
-		if (vassals.count() == 0) {
+		var vassals = Meteor.users.find({lord:user._id}, {fields: {lord:1}});
+		if (vassals.count() === 0) {
 			// reached end
-			self.num_branches--
-			self.traverse_up(user, [])
+			self.num_branches--;
+			self.traverse_up(user, []);
 		} else {
-			// // record siblings
-			// var siblings = vassals.map(function(sib) {
-			// 	return sib._id
-			// })
-			//
-			// vassals.forEach(function(v) {
-			// 	// add siblings info to update_cache
-			// 	var cache_v = _.find(self.update_cache, function(cache) {
-			// 		return (cache.user_id == v._id)
-			// 	})
-			//
-			// 	// check if user is already in update_cache
-			// 	if (cache_v) {
-			// 		// if so add siblings
-			// 		cache_v.siblings = _.without(siblings, v._id)
-			//
-			// 		self.update_cache = _.reject(self.update_cache, function(cache) {
-			// 			return cache.user_id == cache_v.user_id
-			// 		})
-			//
-			// 		self.update_cache.push(cache_v)
-			//
-			// 	} else {
-			// 		// if not add new user
-			// 		self.update_cache.push({
-			// 			user_id: v._id,
-			// 			allies_below: [],
-			// 			allies_above: [],
-			// 			siblings: _.without(siblings, v._id)
-			// 		})
-			// 	}
-			// })
 
-			self.num_branches += vassals.count() - 1
+			self.num_branches += vassals.count() - 1;
 			vassals.forEach(function(vassal) {
-				self.traverse_down(vassal)
-			})
+				self.traverse_down(vassal);
+			});
 		}
-	}
+	};
 
 
 	// traverse through everyone on team up to the king
 	this.traverse_up = function(user, allies_below_array) {
-		check(user, Object)
-		check(allies_below_array, Array)
-		var self = this
+		check(user, Object);
+		check(allies_below_array, Array);
+		var self = this;
 
 		// loop through allies_below_array and add self to everyone's allies_above
 		_.each(allies_below_array, function(id) {
 			if (id != user._id) {
 				// search for user in update_cache (everyone we've already run traverse_up on)
 				var user_below = _.find(self.update_cache, function(cache) {
-					return id == cache.user_id
-				})
+					return id == cache.user_id;
+				});
 
 				if (user_below) {
-					user_below.allies_above = _.union(user_below.allies_above, [user._id])
+					user_below.allies_above = _.union(user_below.allies_above, [user._id]);
 
 					// remove user below
 					self.update_cache = _.reject(self.update_cache, function(cache) {
-						return cache.user_id == user_below.user_id
-					})
+						return cache.user_id == user_below.user_id;
+					});
 
 					// add him again with new allies_above
-					self.update_cache.push(user_below)
+					self.update_cache.push(user_below);
 				}
 			}
-		})
+		});
 
 		// see if user is already in update_cache (another branch could have added him)
 		var u = _.find(self.update_cache, function(cache) {
-			return user._id == cache.user_id
-		})
+			return user._id == cache.user_id;
+		});
 
 		if (u) {
 			// if he is in then merge allies_below
-			u.allies_below = _.union(u.allies_below, _.compact(allies_below_array))
+			u.allies_below = _.union(u.allies_below, _.compact(allies_below_array));
 
 			// remove self
 			self.update_cache = _.reject(self.update_cache, function(cache) {
-				return cache.user_id == user._id
-			})
+				return cache.user_id == user._id;
+			});
 
 			// add with new values
-			self.update_cache.push(u)
+			self.update_cache.push(u);
 
 		} else {
 			// if he's not then add him
@@ -163,42 +140,42 @@ relation_finder = function(user_id) {
 				allies_below: _.compact(allies_below_array),
 				allies_above: [],
 				//siblings: []
-			})
+			});
 		}
 
 		// traverse up
-		var lord = Meteor.users.findOne(user.lord, {fields:{lord:1}})
+		var lord = Meteor.users.findOne(user.lord, {fields:{lord:1}});
 		if (lord) {
 			// add self
-			allies_below_array.push(user._id)
+			allies_below_array.push(user._id);
 
 			// process lord
-			self.traverse_up(lord, allies_below_array)
+			self.traverse_up(lord, allies_below_array);
 		} else {
-			self.reached_top(user)
+			self.reached_top(user);
 		}
-	}
+	};
 
 	// at king
 	this.reached_top = function(user) {
-		var self = this
-		check(self.team, Array)
+		var self = this;
+		check(self.team, Array);
 
-		if (self.num_branches == 0) {
+		if (self.num_branches === 0) {
 			// set king in team
 			// record team in everyone
 
 			self.update_cache = _.map(self.update_cache, function(cache) {
-				cache.allies_below = _.without(cache.allies_below, cache.user_id)
-				cache.allies_above = _.without(cache.allies_above, cache.user_id)
+				cache.allies_below = _.without(cache.allies_below, cache.user_id);
+				cache.allies_above = _.without(cache.allies_above, cache.user_id);
 				//cache.allies = _.union(cache.allies_below, cache.allies_above)
-				return cache
-			})
+				return cache;
+			});
 
 			_.each(self.update_cache, function(cache) {
 				//check(cache.allies, Array)
-				check(cache.allies_below, Array)
-				check(cache.allies_above, Array)
+				check(cache.allies_below, Array);
+				check(cache.allies_above, Array);
 				//check(cache.siblings, Array)
 
 				Meteor.users.update(cache.user_id, {$set: {
@@ -210,14 +187,14 @@ relation_finder = function(user_id) {
 					//team: _.without(self.team, cache.user_id),
 					team: self.team,
 					is_king: cache.user_id == user._id
-				}})
+				}});
 
-				update_vassal_ally_count(cache.user_id)
-			})
+				update_vassal_ally_count(cache.user_id);
+			});
 
-			Cue.addTask('check_for_dominus', {isAsync:false, unique:true}, {})
-			Cue.addTask('cleanupAllKingChatrooms', {isAsync:false, unique:true}, {})
+			Cue.addTask('check_for_dominus', {isAsync:false, unique:true}, {});
+			Cue.addTask('cleanupAllKingChatrooms', {isAsync:false, unique:true}, {});
 		}
-	}
+	};
 
-}
+};
--- game/lib/server/functions/relationships.js
+++ game/lib/server/functions/relationships.js
@@ -10,17 +10,17 @@
 
 
 set_lord_and_vassal = function(winner_id, loser_id) {
-	var fields = {allies_above:1, lord:1, king:1, is_king:1}
+	var fields = {allies_above:1, lord:1, king:1, is_king:1};
 
-	var winner = Meteor.users.findOne(winner_id, {fields:fields})
-	var loser = Meteor.users.findOne(loser_id, {fields:fields})
+	var winner = Meteor.users.findOne(winner_id, {fields:fields});
+	var loser = Meteor.users.findOne(loser_id, {fields:fields});
 
 	if (!winner || !loser) {
-		throw new Meteor.Error('winner or loser not found')
+		throw new Meteor.Error('winner or loser not found');
 	}
 
 	if (winner._id == loser._id) {
-		throw new Meteor.Error('winner and loser are the same in set_lord_and_vassal')
+		throw new Meteor.Error('winner and loser are the same in set_lord_and_vassal');
 	}
 
 	// lost/gained vassal notifications
@@ -33,28 +33,28 @@ set_lord_and_vassal = function(winner_id, loser_id) {
 
 			// make sure we don't send it to winner or loser
 			if (above_loser_lord_id != winner._id && above_loser_lord_id != loser._id) {
-				alert_lostVassal(above_loser_lord_id, loser._id, winner._id)
+				alert_lostVassal(above_loser_lord_id, loser._id, winner._id);
 			}
 		}
-	})
+	});
 
 	_.each(winner.allies_above, function(above_winner_lord_id) {
 		if (_.indexOf(loser.allies_above, above_winner_lord_id) == -1) {
 
 			// make sure we don't send it to winner or loser
 			if (above_winner_lord_id != winner._id && above_winner_lord_id != loser._id) {
-				alert_gainedVassal(above_winner_lord_id, loser._id, winner._id)
+				alert_gainedVassal(above_winner_lord_id, loser._id, winner._id);
 			}
 		}
-	})
+	});
 
 	// send notification to winner
-	alert_gainedVassal(winner._id, loser._id, winner._id)
+	alert_gainedVassal(winner._id, loser._id, winner._id);
 
 	// vassal is no longer a king, he had no lord, now he does
 	// destroy king chatroom
 	if (loser.is_king) {
-		Cue.addTask('destroyKingChatroom', {isAsync:false, unique:true}, {king_id:loser._id})
+		Cue.addTask('destroyKingChatroom', {isAsync:false, unique:true}, {king_id:loser._id});
 	}
 
 	// is loser above winner
@@ -62,238 +62,234 @@ set_lord_and_vassal = function(winner_id, loser_id) {
 	if (_.indexOf(winner.allies_above, loser._id) != -1) {
 
 		// winner is conquering his lord, moving up the tree
-		remove_lord_and_vassal(winner.lord, winner._id)
+		remove_lord_and_vassal(winner.lord, winner._id);
 
 		// loser's lord is now winner's lord
 		if (loser.lord) {
-			remove_lord_and_vassal(loser.lord, loser._id)
-			create_lord_and_vassal(loser.lord, winner._id)
+			remove_lord_and_vassal(loser.lord, loser._id);
+			create_lord_and_vassal(loser.lord, winner._id);
 		}
 
 	// winner is stealing loser from another lord
 	// remove connection between loser and his lord
 	// create notification for old lord that he lost a vassal
 	} else if (loser.lord) {
-		remove_lord_and_vassal(loser.lord, loser._id)
+		remove_lord_and_vassal(loser.lord, loser._id);
 	}
 
 	// create connection between winner and loser
-	create_lord_and_vassal(winner._id, loser._id)
+	create_lord_and_vassal(winner._id, loser._id);
 
 	// is winner a new king
 	// has he conquered his lord?
 	if (winner.king == loser._id) {
-		Meteor.users.update(winner._id, {$set: {is_king:true}})
+		Meteor.users.update(winner._id, {$set: {is_king:true}});
 	}
 
 	// send notification
-	alert_newLord(loser._id, winner._id)
+	alert_newLord(loser._id, winner._id);
 
-	Cue.addTask('enemies_together_check', {isAsync:false, unique:true, delay:500}, {})
-	Cue.addTask('enemy_on_building_check', {isAsync:false, unique:true, delay:500}, {})
-	Cue.addTask('check_for_dominus', {isAsync:false, unique:true, delay:500}, {})
-	Cue.addTask('cleanupAllKingChatrooms', {isAsync:false, unique:true, delay:500}, {})
-}
+	Cue.addTask('enemies_together_check', {isAsync:false, unique:true, delay:500}, {});
+	Cue.addTask('enemy_on_building_check', {isAsync:false, unique:true, delay:500}, {});
+	Cue.addTask('check_for_dominus', {isAsync:false, unique:true, delay:500}, {});
+	Cue.addTask('cleanupAllKingChatrooms', {isAsync:false, unique:true, delay:500}, {});
+};
 
 
 
 // remove lord and vassal connection
 // vassal is now king
 remove_lord_and_vassal = function(lord_id, vassal_id) {
-	check(lord_id, String)
-	check(vassal_id, String)
+	check(lord_id, String);
+	check(vassal_id, String);
 
 	if (lord_id == vassal_id) {
-		throw new Meteor.Error('winner and loser are the same in remove_lord_and_vassal')
+		throw new Meteor.Error('winner and loser are the same in remove_lord_and_vassal');
 	}
 
-	var fields = {allies_above:1, allies_below:1}
-	var lord = Meteor.users.findOne(lord_id, {fields:fields})
-	var vassal = Meteor.users.findOne(vassal_id, {fields:fields})
+	var fields = {allies_above:1, allies_below:1};
+	var lord = Meteor.users.findOne(lord_id, {fields:fields});
+	var vassal = Meteor.users.findOne(vassal_id, {fields:fields});
 
 	if (!lord || !vassal) {
-		throw new Meteor.Error('lord or vassal not found')
+		throw new Meteor.Error('lord or vassal not found');
 	}
 
 	// vassal
 	Meteor.users.update(lord_id, {$pull: {
 		vassals: vassal_id
-	}})
+	}});
 
 	// lord
 	Meteor.users.update(vassal_id, {$set: {
 		lord: null
-	}})
+	}});
 
 	// allies
 	// remove lord and lord's allies_above from 		vassal and vassal's allies_below
 	// allies_above
 	// remove lord and lord's allies_above from 		vassal and vassal's allies_below
-	var pullIds = _.union(lord.allies_above, [lord._id])
-	var pullUsers = _.union(vassal.allies_below, [vassal._id])
-	Meteor.users.update({_id: {$in:pullUsers}}, {$pull: {allies_above:{$in:pullIds}}}, {multi:true})
+	var pullIds = _.union(lord.allies_above, [lord._id]);
+	var pullUsers = _.union(vassal.allies_below, [vassal._id]);
+	Meteor.users.update({_id: {$in:pullUsers}}, {$pull: {allies_above:{$in:pullIds}}}, {multi:true});
 
 	// allies
 	// remove vassal and vassal's allies_below from 	lord and lord's allies_above
 	// allies_below
 	// remove vassal and vassal's allies_below from 	lord and lord's allies_above
-	var pullIds = _.union(vassal.allies_below, [vassal._id])
-	var pullUsers = _.union(lord.allies_above, [lord._id])
-	Meteor.users.update({_id: {$in:pullUsers}}, {$pull: {allies_below:{$in:pullIds}}}, {multi:true})
+	var pullVassalIds = _.union(vassal.allies_below, [vassal._id]);
+	var pullVassalUsers = _.union(lord.allies_above, [lord._id]);
+	Meteor.users.update({_id: {$in:pullVassalUsers}}, {$pull: {allies_below:{$in:pullVassalIds}}}, {multi:true});
 
 	// king
 	// vassal is now king
 	// vassal is now king of everyone below him
-	Meteor.users.update(vassal._id, {$set:{is_king:true}})
-	var setUsers = vassal.allies_below
-	Meteor.users.update({_id:{$in:setUsers}}, {$set:{king:vassal._id}}, {multi:true})
+	Meteor.users.update(vassal._id, {$set:{is_king:true}});
+	var setUsers = vassal.allies_below;
+	Meteor.users.update({_id:{$in:setUsers}}, {$set:{king:vassal._id}}, {multi:true});
 
 	// team
 	// remove vassal and vassal's allies_below from everyone's team
-	var pullIds = _.union(vassal.allies_below, [vassal._id])
-	Meteor.users.update({}, {$pull: {team:{$in:pullIds}}}, {multi:true})
+	var teamIds = _.union(vassal.allies_below, [vassal._id]);
+	Meteor.users.update({}, {$pull: {team:{$in:teamIds}}}, {multi:true});
 	// vassal's team = vassal's new allies_below plus self
-	Meteor.users.update({_id:{$in:pullIds}}, {$set:{team:pullIds}}, {multi:true})
+	Meteor.users.update({_id:{$in:teamIds}}, {$set:{team:teamIds}}, {multi:true});
 
 	// update count of everyone who was changed
-	var ids = _.union([lord_id, vassal_id], lord.team, vassal.team)
+	var ids = _.union([lord_id, vassal_id], lord.team, vassal.team);
 	_.each(ids, function(id) {
 		if (id) {
-			Cue.addTask('updateVassalAllyCount', {isAsync:true, unique:false}, {user_id:id})
+			Cue.addTask('updateVassalAllyCount', {isAsync:true, unique:false}, {user_id:id});
 		}
-	})
-}
+	});
+};
 
 
 
 create_lord_and_vassal = function(lord_id, vassal_id) {
-	check(lord_id, String)
-	check(vassal_id, String)
+	check(lord_id, String);
+	check(vassal_id, String);
 
 	if (lord_id == vassal_id) {
-		console.log('lord '+lord_id)
-		console.log('vassal '+vassal_id)
-		console.error('winner and loser are the same in create_lord_and_vassal')
+		console.log('lord '+lord_id);
+		console.log('vassal '+vassal_id);
+		console.error('winner and loser are the same in create_lord_and_vassal');
 	}
 
-	var fields = {allies_above:1, allies_below:1, king:1, team:1, is_king:1}
-	var lord = Meteor.users.findOne(lord_id, {fields:fields})
-	var vassal = Meteor.users.findOne(vassal_id, {fields:fields})
+	var fields = {allies_above:1, allies_below:1, king:1, team:1, is_king:1};
+	var lord = Meteor.users.findOne(lord_id, {fields:fields});
+	var vassal = Meteor.users.findOne(vassal_id, {fields:fields});
 
 	if (!lord || !vassal) {
-		console.log('lord '+lord_id)
-		console.log('vassal '+vassal_id)
-		console.error('lord or vassal not found')
+		console.log('lord '+lord_id);
+		console.log('vassal '+vassal_id);
+		console.error('lord or vassal not found');
 	}
 
-	check(lord.allies_above, Array)
-	check(lord.allies_below, Array)
-	check(lord.team, Array)
-	check(vassal.allies_above, Array)
-	check(vassal.allies_below, Array)
-	check(vassal.team, Array)
+	check(lord.allies_above, Array);
+	check(lord.allies_below, Array);
+	check(lord.team, Array);
+	check(vassal.allies_above, Array);
+	check(vassal.allies_below, Array);
+	check(vassal.team, Array);
 
 	// vassal must not have a lord
 	// if so call remove first
 	if (vassal.lord) {
-		console.log('lord '+lord_id)
-		console.log('vassal '+vassal_id)
-		console.error('vassal must not have a lord')
+		console.log('lord '+lord_id);
+		console.log('vassal '+vassal_id);
+		console.error('vassal must not have a lord');
 	}
 
 	// set vassal and vassal's allies below's king to king of lord
-	var newKing = null
+	var newKing = null;
 	if (lord.is_king) {
-		newKing = lord._id
+		newKing = lord._id;
 	} else if (lord.king){
-		newKing = lord.king
+		newKing = lord.king;
 	} else {
-		console.log('lord '+lord_id)
-		console.log('vassal '+vassal_id)
-		console.error('lords king is not set')
+		console.log('lord '+lord_id);
+		console.log('vassal '+vassal_id);
+		console.error('lords king is not set');
 	}
-	
+
 	if (newKing) {
-		Meteor.users.update({_id:{$in:vassal.allies_below}}, {$set:{king:newKing}}, {multi:true})
+		Meteor.users.update({_id:{$in:vassal.allies_below}}, {$set:{king:newKing}}, {multi:true});
 	}
 
 	// set lord and remove king
 	Meteor.users.update(vassal_id, {$set: {
 		lord: lord_id,
 		is_king:false
-	}})
+	}});
 
 	// set vassal
 	Meteor.users.update(lord_id, {$addToSet: {
 		vassals: vassal_id
-	}})
+	}});
 
 	// allies_above and allies
 	// add lord and lord's allies_above to 		vassal and vassal's allies below allies_above
 	// add lord and lord's allies_above to		vassal and vassal's allies below allies
-	var addIds = _.union(lord.allies_above, [lord._id])
-	var addUsers = _.union(vassal.allies_below, [vassal._id])
-	Meteor.users.update({_id: {$in:addUsers}}, {$addToSet: {allies_above:{$each:addIds}}}, {multi:true})
+	var addIds = _.union(lord.allies_above, [lord._id]);
+	var addUsers = _.union(vassal.allies_below, [vassal._id]);
+	Meteor.users.update({_id: {$in:addUsers}}, {$addToSet: {allies_above:{$each:addIds}}}, {multi:true});
 
 	// allies_below and allies
 	// add vassal and vassal's allies_below to 	lord and lord's allies above allies_below
 	// add vassal and vassal's allies_below to	lord and lord's allies above allies
-	var addIds = _.union(vassal.allies_below, [vassal._id])
-	var addUsers = _.union(lord.allies_above, [lord._id])
-	Meteor.users.update({_id: {$in:addUsers}}, {$addToSet: {allies_below:{$each:addIds}}}, {multi:true})
+	var addVassalIds = _.union(vassal.allies_below, [vassal._id]);
+	var addVassalUsers = _.union(lord.allies_above, [lord._id]);
+	Meteor.users.update({_id: {$in:addVassalUsers}}, {$addToSet: {allies_below:{$each:addVassalIds}}}, {multi:true});
 
 	// team
 	// add vassal and vassal's allies_below to lord's team
-	var team = _.union(lord.team, [lord._id, vassal._id], vassal.allies_below)
-	Meteor.users.update({_id:{$in:team}}, {$set:{team:team}}, {multi:true})
+	var team = _.union(lord.team, [lord._id, vassal._id], vassal.allies_below);
+	Meteor.users.update({_id:{$in:team}}, {$set:{team:team}}, {multi:true});
 
 	// update count of everyone who was changed
-	var ids = _.union([lord_id, vassal_id], lord.team)
+	var ids = _.union([lord_id, vassal_id], lord.team);
 	_.each(ids, function(id) {
 		if (id) {
-			Cue.addTask('updateVassalAllyCount', {isAsync:true, unique:false}, {user_id:id})
+			Cue.addTask('updateVassalAllyCount', {isAsync:true, unique:false}, {user_id:id});
 		}
-	})
-}
+	});
+};
 
 
 
 Cue.addJob('updateVassalAllyCount', {retryOnError:false, maxMs:1000*60*5}, function(task, done) {
-	update_vassal_ally_count(task.data.user_id)
-	done()
-})
+	update_vassal_ally_count(task.data.user_id);
+	done();
+});
 
 // used for rankings
 update_vassal_ally_count = function(user_id) {
-	check(user_id, String)
+	check(user_id, String);
 
-	var user = Meteor.users.findOne(user_id, {fields: {team:1, vassals:1, allies_above:1, allies_below:1}})
+	var user = Meteor.users.findOne(user_id, {fields: {team:1, vassals:1, allies_above:1, allies_below:1}});
 	if (user) {
+		var num_vassals = 0;
 		if (user.vassals) {
-			var num_vassals = user.vassals.length
-		} else {
-			var num_vassals = 0
+			num_vassals = user.vassals.length;
 		}
 
+		var num_allies_above = 0;
 		if (user.allies_above) {
-			var num_allies_above = user.allies_above.length
-		} else {
-			var num_allies_above = 0
+			num_allies_above = user.allies_above.length;
 		}
 
+		var num_allies_below = 0;
 		if (user.allies_below) {
-			var num_allies_below = user.allies_below.length
-		} else {
-			var num_allies_below = 0
+			num_allies_below = user.allies_below.length;
 		}
 
+		var num_team = 0;
 		if (user.team) {
-			var num_team = user.team.length
-		} else {
-			var num_team = 0
+			num_team = user.team.length;
 		}
 
-		Meteor.users.update(user_id, {$set: {num_team:num_team, num_vassals: num_vassals, num_allies_above: num_allies_above, num_allies_below: num_allies_below}})
-		Cue.addTask('dailystats_num_allies', {isAsync:true, unique:false}, {user_id:user_id})
+		Meteor.users.update(user_id, {$set: {num_team:num_team, num_vassals: num_vassals, num_allies_above: num_allies_above, num_allies_below: num_allies_below}});
+		Cue.addTask('dailystats_num_allies', {isAsync:true, unique:false}, {user_id:user_id});
 	}
-}
+};
