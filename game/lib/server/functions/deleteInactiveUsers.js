f25ae394c23f7b87e29fae1e869c2270055d1d8a
dan <danphi@gmail.com>
2015-03-29 19:47:05 -0700

    update lastActive when user goes idle

--- game/lib/server/functions/deleteInactiveUsers.js
+++ game/lib/server/functions/deleteInactiveUsers.js
@@ -132,6 +132,10 @@ UserStatus.events.on('connectionLogout', function(fields) {
     updateUserLastActive(fields.userId);
 });
 
+UserStatus.events.on('connectionIdle', function(fields) {
+    updateUserLastActive(fields.userId);
+});
+
 var updateUserLastActive = function(userId) {
     Meteor.users.update(userId, {$set:{lastActive:new Date()}});
 };
